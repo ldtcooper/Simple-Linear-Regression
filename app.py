df1e2ccdac7b52cc02fa83764bd46702b7b78f79
Logan Cooper <ldtcoop@gmail.com>
2019-09-15 21:54:38 -0700

    Create unstyled frontend for API (#3)
    * Add simple HTML page
    
    * Serve HTML page
    
    * Add normalize.css
    
    * Add upload input
    
    * Add favicon
    
    * Include useful libraries
    
    * Set up basic file handler
    
    * Fully parse uploaded CSV
    
    * Connect frontend to backend
    
    * Handle api call
    
    * Handle error messages
    
    * Display results after submitting data
    
    * Add readme
    
    * Change backend function to something sensible
    
    * Serve normalize.css through CDN

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-from flask import Flask, request, abort
+from flask import Flask, request, abort, url_for, redirect
 from regression import regress
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path = '')
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+@app.route("/", methods = ['GET'])
+def homepage():
+    return app.send_static_file('index.html')
 
 @app.route("/regress", methods = ['POST'])
 def send_regression():
     arguments = request.get_json()
+    print(arguments)
     dependent_var = arguments['dep']
     independent_var = arguments['ind']
     different_data_lengths = len(dependent_var) != len(independent_var)
---
+++ static/index.js
@@ -0,0 +1,111 @@
+const state = {};
+
+const setMessage = (id, newClass, msg) => {
+    const errorTag = document.getElementById(id);
+    errorTag.className = newClass;
+    errorTag.textContent = msg;
+};
+
+const setError = (msg) => setMessage('error', 'upload__error', msg);
+const removeError = (msg) => setMessage('error', 'upload__error--invisible', msg);
+const setSuccess = (msg) => setMessage('success', 'upload__success', 'Data uploaded successfully!');
+const setValues = ({ slope, intercept }) => {
+    document.getElementById('coefficient').textContent = slope;
+    document.getElementById('constant').textContent = intercept;
+    document.getElementById('values').className = 'results__regression-values';
+};
+
+const handleFileUpload = (e) => {
+    removeError();
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.readAsText(file, 'UTF-8');
+
+    reader.onload = (evt) => {
+        Papa.parse(evt.target.result, {
+            dynamicTyping: true,
+            skipEmptyLines: true,
+            complete: (r) => {
+                const csv = r.data.slice(0,2); // we can only handle two rows
+
+                // scan csv data for problems
+                const lengthDifference = csv[0].length !== csv[1].length;
+                if (lengthDifference) {
+                    setError('Error: Your data series are not the same length. Plese check your data and try again.');
+                    return;
+                }
+
+                const verifyBody = (data) => {
+                    const allNumbers = data.map((el) => {
+                        el = Number(el);
+                        if (isNaN(el)) {
+                            const numberError = 'All non-title data points must be numbers';
+                            setError(numberError);
+                            document.getElementById('file-upload').value = '';
+                            throw new Error(numberError);
+                        }
+
+                        return el;
+                    });
+
+                    return allNumbers;
+                };
+
+                // package data for API
+                const requestBody = csv.reduce((acc, val, i) => {
+                    const bodyData = verifyBody(val.slice(1));
+                    const key = i === 0 ? 'dep' : 'ind';
+                    acc[key] = bodyData;
+                    return acc;
+                }, {});
+
+                // package data for chart
+                const chartData = csv[0].slice(1).map((x, i) => {
+                    const y = csv[1][i];
+                    return { x, y };
+                });
+                const seriesNames = { x: csv[0][0], y: csv[1][0] };
+
+                state.requestBody = requestBody;
+                state.chartData = chartData;
+                state.seriesNames = seriesNames;
+                document.getElementById('submit-btn').disabled = false;
+                setSuccess();
+            }
+        });
+    };
+
+    reader.onerror = (evt) => {
+        setError('Error reading CSV File');
+    };
+};
+
+const handleDataSubmission = (e) => {
+    if (!state.requestBody) {
+        setError('Please upload a file');
+    } else {
+        fetch('/regress', {
+            method: 'POST',
+            body: JSON.stringify(state.requestBody),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }).then((r) => {
+            if (r.ok) {
+                r.json().then((json) => {
+                    state.regressionLine = json;
+                    setValues(json);
+                });
+            } else if (r.status === 400) {
+                setError('It appears that you have uploaded some invalid data. Please double-check your data and try again');
+            } else {
+                setError('An API error occured. Please try again later.');
+            }
+        });
+    }
+};
+
+window.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('file-upload').onchange = handleFileUpload;
+    document.getElementById('submit-btn').onclick = handleDataSubmission;
+});
